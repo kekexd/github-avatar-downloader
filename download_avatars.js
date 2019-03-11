ea187e229e5d2570011a6602f401a612f10058f1
Xia Dong <dongxiapp@gmail.com>
2019-03-11 23:46:10 +0000

    add user-agent and token

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,22 @@
 var request = require('request');
+var token = require('./secret')
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+    },
+    qs:{
+      access_token: token.GITHUB_TOKEN
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
